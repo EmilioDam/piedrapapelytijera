0b07f703ced9337145f3c5e5193ca55441d8092e
EmilioDam <=>
2024-01-22 22:05:00 +0100

    Comprobación del ganador

--- piedrapapelotijera.py
+++ piedrapapelotijera.py
@@ -5,7 +5,7 @@ print("Piedra, papel o tijera, V0.3")
 print("1 = piedra")
 print("2 = papel")
 print("3 = tijera")
-tirada = input("Escoge tu tirada: ")
+tirada = int(input("Escoge tu tirada: "))
 
 tiradaordenador = random.randint(1, 3)
 
@@ -18,3 +18,23 @@ elif tiradaordenador == 3:
     print("El ordenador ha sacado tijera")
     
 
+if tirada == 1 and tiradaordenador == 1:
+    print ("Empate")
+elif tirada == 1 and tiradaordenador == 2:
+    print ("Gana la maquina")
+elif tirada == 1 and tiradaordenador == 3:
+    print ("El jugador gana")
+
+elif tirada == 2 and tiradaordenador == 1:
+    print ("El jugador gana")
+elif tirada == 2 and tiradaordenador == 2:
+    print ("Empate")
+elif tirada == 2 and tiradaordenador == 3:
+    print ("Gana la maquina")
+
+elif tirada == 3 and tiradaordenador == 1:
+    print ("Gana la maquina")
+elif tirada == 3 and tiradaordenador == 2:
+    print ("El jugador gana")
+elif tirada == 3 and tiradaordenador == 3:
+    print ("Empate")
